a78bdcbbe7c9650c55b4461f7deefae8da815b72
ddtddt <ddtddt@piwigo.org>
2015-01-24 07:06:47 +0000

    [extensions] - write_metadata - add fr_CA - Quebec - Québécois Thanks to TiOger
    git-svn-id: http://piwigo.org/svn/extensions/write_metadata@30904 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/fr_CA/plugin.lang.php
@@ -0,0 +1,5 @@
+<?php
+$lang['Write metadata'] = 'Écrire les métadonnées';
+$lang['Write metadata in progress...'] = 'Écriture des métadonnées en cours...';
+$lang['Metadata written into file'] = 'Métadonnées écrites dans le(s) fichier(s)'; 
+?>
