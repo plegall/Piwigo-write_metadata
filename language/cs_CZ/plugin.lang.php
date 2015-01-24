08b27b5d9d4a26260f754df378b3b59e5d4d0e39
ddtddt <ddtddt@piwigo.org>
2015-01-24 07:03:50 +0000

    [extensions] - write_metadata - add cs_CZ - Česky – Czech Thanks to webprostor
    git-svn-id: http://piwigo.org/svn/extensions/write_metadata@30903 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/cs_CZ/plugin.lang.php
@@ -0,0 +1,5 @@
+<?php
+$lang['Write metadata'] = 'Zápis metadat';
+$lang['Write metadata in progress...'] = 'Zápis metadat právě probíhá...';
+$lang['Metadata written into file'] = 'Metadata byla zapsána do souboru(ů)';
+?>
