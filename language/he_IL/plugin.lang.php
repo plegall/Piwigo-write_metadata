fb711a80e005f1abd9f6ede6160e2b7bbf2add8a
Piwigo-TranslationTeam <translate-github@piwigo.org>
2024-12-20 22:49:31 +0100

    Update he_IL, thanks to epifeny

---
+++ language/he_IL/plugin.lang.php
@@ -0,0 +1,10 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Metadata written into file'] = 'מטא-נתונים נכתבו לקובץ/קבצים';
+$lang['Write metadata'] = 'כתוב מטא-נתונים';
+$lang['Write metadata in progress...'] = 'המטא-נתונים בתהליך כתיבה...';
