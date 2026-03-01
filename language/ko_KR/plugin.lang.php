284ce6c31db5a1356fc8075d651d72e8f5b4fff4
Piwigo-TranslationTeam <translate-github@piwigo.org>
2026-03-01 05:35:34 +0100

    Update ko_KR, thanks to yjchoi

---
+++ language/ko_KR/plugin.lang.php
@@ -0,0 +1,10 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Metadata written into file'] = '파일에 기록된 메타데이터';
+$lang['Write metadata'] = '메타데이터 기록 중';
+$lang['Write metadata in progress...'] = '메타데이터 기록 진행 중...';
